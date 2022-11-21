f49621c0e1e1a093178cfadd2471fa3e6d740ce4
lenaSamaha <lena.sama7a@gmail.com>
2022-11-22 00:29:17 +0200

    Added the solution of myStack

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace myStacknamespace
+{
+    class MyStack<T>
+    {
+        private T[]? stack;
+        private int count = -1;
+        private readonly int maxSize;
+        public MyStack(int maxSize = 20) //default is 20, if user doesn't enter it
+        {
+            this.maxSize = maxSize;
+            //Console.WriteLine(maxSize);
+            stack = new T[maxSize];
+        }
+
+        public void Push(T value)
+        { 
+            if(count+1 >= maxSize)
+            {
+                Console.WriteLine("over flow stack, It can't add anything ");
+
+            }
+            else
+            {
+                stack[++count] = value;
+            }
+        }
+
+        public T Pop()
+        {
+            if (count == -1)
+            {
+                Console.WriteLine("under flow stack, please push to it first");
+                // throw new InvalidOperationException();
+                return default(T);
+            }
+            else
+                return stack[count--];
+        }
+
+        public T Peak()
+        {
+            if (count == -1)
+            {
+                Console.WriteLine("under flow stack, please push to it first");
+                return default(T);
+                //throw new InvalidOperationException();
+            }
+            else
+                return stack[count];
+        }
+
+        public void Print()
+        {
+            Console.Write("Stack contains:  ");
+            for (int i = 0; i <= count; i++)
+            {
+                if (i == count )
+                {
+                    Console.Write(stack[i]);
+                    Console.WriteLine();
+
+                }
+                else
+                    Console.Write(stack[i] + ", ");
+
+            }
+        }
+
+        public void Clear()
+        {
+            Console.WriteLine("Stack after clear:");
+
+            for (int i = 0; i < stack.Length; i++)
+            {
+                stack[i] = default(T);
+                count = -1;
+                if(i == (stack.Length-1))
+                    Console.WriteLine();
+                else
+                    Console.Write(stack[i]);
+
+            }
+        }
+    }
+}
---
+++ MyStack/Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace myStacknamespace
+{
+    internal class Test
+    {
+        public static void Main()
+        {
+            MyStack<String> names = new MyStack<String>();
+            names.Push("Leena");
+            names.Push("foothill");
+            names.Print();
+            Console.WriteLine($"top value is: {names.Pop()}");
+            names.Print();
+            Console.WriteLine($"top value is: {names.Peak()}");
+            names.Print();
+            names.Clear();
+            names.Push("hello");
+
+            MyStack<double> values = new MyStack<double>(5);
+            //Notice that I set maxSize = 5
+            values.Pop(); // underflow
+            values.Push(1.5);
+            values.Push(2);
+            values.Push(3.3);
+            values.Push(4.7);
+            values.Push(5);
+            values.Print();
+            values.Push(6); //overflow because maxSize=5
+            values.Print();
+            values.Pop();
+            values.Print();
+            values.Clear();
+
+            values.Push(1111);
+            values.Print();
+
+        }
+
+
+    }
+}
